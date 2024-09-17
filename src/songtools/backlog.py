e4cbbb619d28704bf41b83a04e7884d0bbada26c
Jake Balas <jake@balas.io>
2024-09-17 14:35:58 +0200

    backlog - add functionality to remove empty folders

--- src/songtools/backlog.py
+++ src/songtools/backlog.py
@@ -1,3 +1,31 @@
-def clean_backlog_folder():
-    # TODO: implement
-    return None
+from pathlib import Path
+
+
+def clean_backlog_folder(backlog_folder: Path) -> None:
+    """Take the backlog folder and clean it.
+    It will:
+     - Remove all empty folders recursively
+
+    :param Path backlog_folder: Root path to the backlog folder
+    """
+    remove_empty_folders(backlog_folder)
+
+
+def remove_empty_folders(root_path: Path) -> None:
+    """Recursively remove all empty folders.
+    It stops at 100 iterations of nesting to prevent any weird infinite loops.
+
+    :param Path root_path: Root path to start the search
+    """
+    empties_exists = True
+    nest = 0
+    max_nested = 100
+    while empties_exists and nest < max_nested:
+        empties_exists = False
+        for folder in sorted(
+            root_path.rglob("*"), key=lambda p: len(p.parts), reverse=True
+        ):
+            if folder.is_dir() and not any(folder.iterdir()):
+                folder.rmdir()
+                empties_exists = True
+        nest += 1
--- src/songtools/tests/test_backlog.py
+++ src/songtools/tests/test_backlog.py
@@ -1,14 +1,25 @@
-from songtools.conftest import create_test_mp3_data, get_test_folder, cleanup_tst_folder
+from songtools.conftest import get_test_folder, cleanup_tst_folder
 
 from songtools.backlog import clean_backlog_folder
 
 
-def test_backlog_folder_gets_cleaned():
-    tst_folder = get_test_folder()
-    mp3 = create_test_mp3_data()
-    a = tst_folder / "tstmp3.mp3"
-    with open(a, "wb") as f:
-        f.write(mp3.getvalue())
-    clean_backlog_folder()
+def _prepare_dirty_backlog_folder():
+    """Simulates a backlog folder that is in a proper disarray and needs cleaning.
+
+    :rtype: Path
+    :return: Test folder root path.
+    """
+    root_folder = get_test_folder()
+    empty_folder_a = root_folder / "empty_folder_a"
+    empty_folder_a.mkdir(exist_ok=True)
+    empty_folder_a2 = empty_folder_a / "empty_folder_a2"
+    empty_folder_a2.mkdir(exist_ok=True)
+
+    return root_folder
+
+
+def test_backlog_removes_empty_dirs():
+    tst_folder = _prepare_dirty_backlog_folder()
+    clean_backlog_folder(tst_folder)
+    assert not (tst_folder / "empty_folder_a").exists()
     cleanup_tst_folder()
-    assert True
