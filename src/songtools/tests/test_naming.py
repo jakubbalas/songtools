bc92acb567f23f6216bf6012be9bb629d7ae5cee
Jake Balas <jake@balas.io>
2024-09-23 16:24:09 +0200

    Test special chars replacement

--- src/songtools/tests/test_naming.py
+++ src/songtools/tests/test_naming.py
@@ -1,4 +1,4 @@
-from songtools.naming import has_cyrillic
+from songtools.naming import has_cyrillic, remove_special_characters
 
 
 def test_has_cyrillic():
@@ -6,3 +6,11 @@ def test_has_cyrillic():
     assert not has_cyrillic(text)
     text = "Привет, мир!"
     assert has_cyrillic(text)
+
+
+def test_remove_special_characters():
+    text = "Hi?my:name.is\x19a\x01big|j*dp>and<this/is\\my_crib.."
+    assert (
+        "Hi my name is a big jxdp and this is my crib  "
+        == remove_special_characters(text)
+    )
